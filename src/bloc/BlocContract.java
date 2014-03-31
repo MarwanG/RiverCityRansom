b802d65f919e4f8be9e3fe13468573f344eb5802
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2014-03-31 21:13:06 +0200

    added check invariants

--- src/bloc/BlocContract.java
+++ src/bloc/BlocContract.java
@@ -7,6 +7,32 @@ public class BlocContract extends BlocDecorator {
 	
 	public BlocContract(BlocI b){
 		super(b);
+		
+	}
+	
+	public void checkInvariant(){
+		if(this.hasTreasure() && this.getTreasure() == null){
+			Contractor.defaultContractor().invariantError("BlocContract", "Treasure changed in variant");
+		}
+		if(!this.hasTreasure() && this.getTreasure()!= null){
+			Contractor.defaultContractor().invariantError("BlocContract", "Treasure changed in variant");		
+		}
+		if(this.isPit() == this.isEmpty()){
+			Contractor.defaultContractor().invariantError("BlocContract", "Pit and empty problem");		
+		}
+	}
+	
+	public void init(boolean b, Object obj){
+		super.init(b, obj);
+		checkInvariant();
+		if(b && !this.isEmpty())
+			Contractor.defaultContractor().postconditionError("BlocContract", "init", "empty defined wrong");
+		if(obj == null && this.hasTreasure())
+				Contractor.defaultContractor().postconditionError("BlocContract", "init", "treasure not defined");
+		if(obj != null && !this.hasTreasure())
+			Contractor.defaultContractor().postconditionError("BlocContract", "init", "treasure should be defined");
+		if(obj != null && this.getTreasure()!=obj)
+			Contractor.defaultContractor().postconditionError("BlocContract", "init", "treasure not properly defined");
 	}
 	
 	public Object getTreasure(){
@@ -24,7 +50,14 @@ public class BlocContract extends BlocDecorator {
 			Contractor.defaultContractor().preconditionError("BlocContract", "removeTreasure", "doesnt has treasure");
 			return null;
 		}else{
-			return delegate.removeTreasure();
+			checkInvariant();
+			Object obj =  delegate.removeTreasure();
+			checkInvariant();
+			if(this.getTreasure() != null)
+				Contractor.defaultContractor().postconditionError("BlocContract", "removeTreasure", "treasure not removed");
+			if(!this.hasTreasure())
+				Contractor.defaultContractor().postconditionError("BlocContract", "removeTreasure", "has treasure wrong");
+			return obj;
 		}
 	}
 }
