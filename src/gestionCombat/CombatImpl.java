5349de7943895b7dcba7a13af3d23d66f625f694
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2014-04-15 11:33:22 +0200

    inrange and freeze

--- src/gestionCombat/CombatImpl.java
+++ src/gestionCombat/CombatImpl.java
@@ -39,6 +39,12 @@ public class CombatImpl implements CombatI {
 	private ArrayList<Integer> gangZ;
 	
 	
+	private int ryanF;
+	private int alexF;
+	private int slickF;
+	private ArrayList<Integer> gangF;
+	
+	
 	public CombatImpl(int x , int y , int z){
 		init(x,y,z);
 	}
@@ -56,6 +62,7 @@ public class CombatImpl implements CombatI {
 		for(int i = 0 ; i < nbGangsters ; i++){
 			//MISSING TERRAIN STUFF
 			gang.add(new GangsterImpl("Scumbag"));
+			gangF.add(0);
 		}
 		//how do u want to save the freeze.
 		ryanX = 0;
@@ -67,6 +74,10 @@ public class CombatImpl implements CombatI {
 		ryanZ = 0;
 		alexZ = 0;
 		slickZ = 0;
+		
+		ryanF = 0;
+		alexF = 0;
+		slickF = 0;
 	}
 	
 	@Override
@@ -131,13 +142,93 @@ public class CombatImpl implements CombatI {
 	}
 	@Override
 	public int freeze(PersonnageI p) {
-		// TODO Auto-generated method stub
+		if(p.getNom().equals("alex")){
+			return alexF;
+		}else if(p.getNom().equals("ryan")){
+			return ryanF;
+		}else if(p.getNom().equals("slick")){
+			return slickF;
+		}else if(p.getNom().contains("Gangster_")){
+			int id = Integer.parseInt((p.getNom().charAt((p.getNom().length()-1))+""));
+			if(id > nbGangsters){
+				return -1;
+			}else{
+				return gangF.get(id);
+			}
+		}
 		return 0;
 	}
 	@Override
 	public boolean inRange(PersonnageI p1, PersonnageI p2) {
-		// TODO Auto-generated method stub
-		return false;
+		int x1 = 0;
+		int y1 = 0;
+		int z1 = 0;
+		int x2 = 0;
+		int y2 = 0;
+		int z2 = 0;
+		
+		
+		if(p1.getNom().equals("alex")){
+			x1 = alexX;
+			y1 = alexY;
+			z1 = alexZ;
+		}else if(p1.getNom().equals("ryan")){
+			x1 = ryanX;
+			y1 = ryanY;
+			z1 = ryanZ;
+		}else if(p1.getNom().equals("slick")){
+			x1 = slickX;
+			y1 = slickY;
+			z1 = slickZ;
+		}else if(p1.getNom().contains("Gangster_")){
+			int id = Integer.parseInt((p1.getNom().charAt((p1.getNom().length()-1))+""));
+			if(id > nbGangsters){
+				return false;
+			}else{
+				x1 = gangX.get(id);
+				y1 = gangY.get(id);
+				z1 = gangZ.get(id);
+			}
+		}
+		
+		if(p2.getNom().equals("alex")){
+			x2 = alexX;
+			y2 = alexY;
+			z2 = alexZ;
+		}else if(p2.getNom().equals("ryan")){
+			x2= ryanX;
+			y2 = ryanY;
+			z2 = ryanZ;
+		}else if(p2.getNom().equals("slick")){
+			x2 = slickX;
+			y2 = slickY;
+			z2 = slickZ;
+		}else if(p2.getNom().contains("Gangster_")){
+			int id = Integer.parseInt((p2.getNom().charAt((p2.getNom().length()-1))+""));
+			if(id > nbGangsters){
+				return false;
+			}else{
+				x2 = gangX.get(id);
+				y2 = gangY.get(id);
+				z2 = gangZ.get(id);
+			}
+		}
+		
+		if(x1 == x2 && y1 == y2 && ((z1 == z2+1) || (z1+1 == z2))){
+			return true;
+		}else if(x1 == x2 && z1 == z2 && ((y1 == y2+1) || (y1+1 == y2))){
+			return true;
+		}else if(z1 == z2 && y1 == y2 && ((x1 == x2+1) || (x1+1 == x2))){
+			return true;
+		}else if(z1 == z2 && ((x1 == x2+1) || (x1+1 == x2)) && ((y1 == y2+1) || (y1+1 == y2))){
+			return true;
+		}else if(x1 == x2 && ((z1 == z2+1) || (z1+1 == z2)) && ((y1 == y2+1) || (y1+1 == y2))){
+			return true;
+		}else if(y1 == y2 && ((x1 == x2+1) || (x1+1 == x2)) && ((z1 == z2+1) || (z1+1 == z2))){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	@Override
 	public void step(Commande c1, Commande c2) {
